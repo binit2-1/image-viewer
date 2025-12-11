1dbcdc6e32e181e4135762633076aaaf1d5f9320
binit <binitgupta.1711@gmail.com>
2025-12-12 00:43:54 +0530

    Update Makefile Window Opening

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ CC = gcc
 all: image-viewer
 
 image-viewer:image-viewer.c
-	$(CC) -o image-viewer image-viewer.c
+	$(CC) -Wall -Wextra -fpermissive -o image-viewer image-viewer.c `sdl2-config --cflags --libs`
--- image-viewer.c
+++ image-viewer.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-int main() {
-    printf("Hello, Image Viewer!\n");
-    return 0;
+int main()
+{
+    const char *ptitle = "Image Viewer";
+    int x = SDL_WINDOWPOS_CENTERED;
+    int y = SDL_WINDOWPOS_CENTERED;
+    int w  = 800;
+    int h  = 400;
+    Uint32 flags = 0;
+    SDL_Window *pwindow = SDL_CreateWindow(ptitle,
+                                 x, y, w,
+                                 h, flags);
+    
+    
+    SDL_Surface *pscreen = SDL_GetWindowSurface(pwindow);
+    SDL_FillRect(pscreen, NULL, SDL_MapRGB(pscreen->format, 0, 0, 255));
+    SDL_UpdateWindowSurface(pwindow);
+    SDL_Delay(3000);
+
+    
 }
